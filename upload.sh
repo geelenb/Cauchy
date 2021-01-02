c3b899df89cdb600023066c10e454f9b1716014d
bgeelen <bram.geelen@esat.kuleuven.be>
2021-01-02 12:08:30 +0100

    split into different files

--- upload.sh
+++ upload.sh
@@ -1,1 +1,5 @@
-rsync -v * esat:public_html/cauchy/
+sed s/index.js/index.min.js/g -i sed_backup index.html
+terser index.js -o index.min.js
+rsync -vr * esat:public_html/cauchy/
+rm index.min.js
+sed s/index.min.js/index.js/g -i sed_backup index.html
